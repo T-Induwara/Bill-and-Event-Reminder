8cd37ba0a711c3939ab41dd3770138eba173eab5
t-induwara <tharusha.induwar@gmail.com>
2023-05-31 11:12:11 +0530

    Nav menu completed for all devices

---
+++ assets/js/main-script.js
@@ -0,0 +1,16 @@
+console.log("Script loaded");
+
+var navMenu = document.getElementById("nav-list");
+var mOpenBtn = document.getElementById("op-btn");
+var mCloseBtn = document.getElementById("co-btn");
+
+mOpenBtn.addEventListener("click",function(){
+    navMenu.style.display = "flex";
+    mOpenBtn.style.display = "none";
+    mCloseBtn.style.display = "block";
+});
+mCloseBtn.addEventListener("click",function(){
+    navMenu.style.display = "none";
+    mOpenBtn.style.display = "block";
+    mCloseBtn.style.display = "none";
+});
