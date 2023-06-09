4b65e37b2009942063eee937f6fba83153b6ca76
Savindushehan <savindushehansamaraweera@gmail.com>
2023-06-09 18:44:32 +0530

    changed image urls on user dashboard page

--- assets/user-dashboard.php
+++ assets/user-dashboard.php
@@ -266,13 +266,13 @@
             <div class="category">
               <a id="but" href="bill-types.php">  
                    <div class="subbutton" >
-                   <img id="img1" alt="image" src="/assets/images/user-dashboard/add-bills.webp">
+                   <img id="img1" alt="image" src="images/user-dashboard/add-bills.webp">
                    </div>
                    <p id="dis">Add Bills</p> 
               </a>    
               <a id="but" href="event-types.php">  
                    <div class="subbutton" >
-                   <img id="img1" alt="image" src="/assets/images/user-dashboard/add-events.webp">
+                   <img id="img1" alt="image" src="images/user-dashboard/add-events.webp">
                    </div>
                    <p id="dis">Add Events</p> 
               </a>    
