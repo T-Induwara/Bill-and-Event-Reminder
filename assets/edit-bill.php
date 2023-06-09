98cffb45f564853b224258f8fa764764d2706d9f
Malith Gihan <125493545+MalithGihan@users.noreply.github.com>
2023-06-09 17:18:52 +0530

    Created Bill and Event edit pages

---
+++ assets/edit-bill.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>User Dashboard - RemindMeister</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <script type="text/js" src="js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/main-style.css">
+        <style>
+            :root{
+                --primary: #001f3f;
+                --accent: ;
+                --bg: #f8f8f8;
+                --txt: #333333;
+                --cta: #ff851b;
+            }
+            body{
+                margin:0 !important;
+                padding:0 !important;
+                background-color: var(--bg);
+            }
+            @font-face{
+                font-family:Poppins-R;
+                src:url('font/Poppins-Regular.ttf');
+            }
+            @font-face{
+                font-family:Poppins-S;
+                src:url('font/Poppins-SemiBold.ttf');
+            }
+            /*Default css for mobile*/
+            .dashboard-header{
+                padding:2em 3em 2em 3em;
+            }
+            .usr-col{
+                margin-top:1rem;
+                flex-direction:row;
+                justify-content:center;
+                align-items: center;
+            }
+            .usr-col-details{
+                flex-direction: column;
+                margin-left:0.5rem;
+            }
+            .usr-image{
+                margin-top:-0.5rem;
+                width:15%;
+            }
+            .title-col h1,.usr-name{
+                font-family:Poppins-S;
+            }
+            .title-col h1{
+                font-size:40px;
+                text-align: center;
+            }
+            .usr-name{
+                font-size: 25px;
+            }
+            .usr-mail{
+                margin-top:-0.8rem;
+                font-family: Poppins-R;
+                font-size: 15px;
+            }
+
+
+          
+
+        
+          
+            
+
+            /*CSS for tablet*/
+            @media only screen and (min-width:768px){
+                .dashboard-header{
+                    padding:2em 2em 2em 2em;
+                }
+                .usr-col{
+                    margin-top:0.5rem;
+                    justify-content:end;
+                }
+                .usr-col-details{
+                    flex-direction: column;
+                    margin-left:0.5rem;
+                }
+                .usr-image{
+                    width:15%;
+                }
+                .title-col h1{
+                    font-size:50px;
+                    text-align: left;
+                }
+                .usr-name{
+                    font-size: 28px;
+                }
+                .usr-mail{
+                    margin-top:-1rem;
+                    font-size: 15px;
+                }
+
+
+
+                
+                
+
+                
+            }
+
+            /*CSS for desktop*/
+            @media only screen and (min-width:1280px){
+                .dashboard-header{
+                    padding:2em 3em 2em 3em;
+                }
+                .usr-col-details{
+                    margin-left:1rem;
+                }
+                .usr-image{
+                    width:8%;
+                }
+                .title-col h1{
+                    font-size:50px;
+                }
+                .usr-name{
+                    font-size: 28px;
+                }
+                .usr-mail{
+                    margin-top:-1rem;
+                    font-size: 15px;
+                }
+
+               
+
+
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <div class="nav-container">
+                <div class="nav-logo"><a href="../index.html">REMINDMEISTER</a></div>
+                <div class="nav-list" id="nav-list">
+                    <a href="../index.html">Home</a>
+                    <a href="aboutus.html">About Us</a>
+                    <a href="contact.php">Contact Us</a>
+                    <a href="#" class="nav-log-btn"><b>Log out</b></a>
+                </div>
+                <div class="m-nav-btn">
+                    <img src="images/header/m-open.webp" alt="m open btn" class="op-btn" id="op-btn">
+                    <img src="images/header/m-close.webp" alt="m close btn" class="co-btn" id="co-btn">
+                </div>
+            </div>
+        </header>
+        <main>
+            <?php
+            ?>   
+        </main>
+        <footer>
+            <div class="footer-container">
+                <div class="foo-desc">
+                    <p>RemindMeister is your ultimate companion for staying organized and on top of your bills and events. With its user-friendly interface, smart reminders, and seamless integration, managing your schedule has never been easier. Say goodbye to missed deadlines and hello to a more efficient and stress-free life.</p>
+                    <h1>REMINDMEISTER</h1>
+                </div>
+                <div class="foo-urls">
+                    <div class="foo-list">
+                        <a href="../index.html">Home</a>
+                        <a href="aboutus.html">About Us</a>
+                        <a href="contact.php">Contact Us</a>
+                        <a href="#" class="nav-log-btn"><b>Log out</b></a>
+                    </div>
+                    <img src="images/footer/Saly-12.webp" alt="footer image" class="footer-img">
+                </div>
+                <div class="foo-social">
+                    <h3>Our Social Medias</h3>
+                    <div class="social-icons">
+                        <img src="images/footer/social-icons/ic_baseline-facebook.webp" alt="facebook icon">
+                        <img src="images/footer/social-icons/mdi_youtube.webp" alt="youtube icon">
+                        <img src="images/footer/social-icons/mdi_linkedin.webp" alt="linkedin icon">
+                        <img src="images/footer/social-icons/mdi_twitter.webp" alt="twitter icon">
+                        <img src="images/footer/social-icons/ri_whatsapp-fill.webp" alt="whatsapp icon">
+                        <img src="images/footer/social-icons/ri_instagram-fill.webp" alt="instragram icon">
+                    </div>
+                    <h3 class="foo-email-h">E-mail</h3>
+                    <p><a href="mailto:hello@remindmeister.com">HELLO@REMINDMEISTER.COM</a></p>
+                    <h3 class="foo-phone-h">Phone</h3>
+                    <p><a href="tel+94112546782">+94 11 2546782</a></p>
+                </div>
+            </div>
+        </footer>
+        <script src="js/main-script.js"></script>
+    </body>
+</html>
